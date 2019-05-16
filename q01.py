0c80f6dfae42d7fe820405f039f4a7f5016fceb8
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-15 20:54:31 -0500

    Add files via upload

--- q01.py
+++ q01.py
@@ -1,3 +1,16 @@
 ##
 ## Imprima la suma de la segunda columna.
 ##
+text = []
+with open('data.csv', 'rt') as f:
+    text += f.readlines()
+
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+
+x = 0
+
+for line in text:
+    x = x + int(line[1])
+
+print(x)
