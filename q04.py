2e7ccb63b5d9aabc6a71dd3db9877af95eb04804
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-17 20:54:10 -0500

    Add files via upload

--- q04.py
+++ q04.py
@@ -14,3 +14,15 @@
 ## 11,2
 ## 12,3
 ##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+        
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+text = [z+[z[2].split('-')[1]] for z in text]
+text2 = [line[-1] for line in text]
+text3 = list(set(text2))
+text3.sort()
+for letra in text3:
+    print('{},{}'.format(letra,text2.count(letra)))
