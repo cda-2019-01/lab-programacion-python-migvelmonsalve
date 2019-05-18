e317dc5848b4d595b911c343e45bddaabdb6fb4e
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-17 20:34:02 -0500

    Add files via upload

--- q02.py
+++ q02.py
@@ -8,3 +8,14 @@
 ## D,6
 ## E,14
 ##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+        
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+text2 = [line[0] for line in text]
+text3 = list(set(text2))
+text3.sort()
+for letra in text3:
+    print('{},{}'.format(letra,text2.count(letra)))
