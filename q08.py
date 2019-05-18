6f4c1b2108a2ad5a1d11013ba5ba03e1f75c5a3a
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-18 17:31:34 -0500

    Add files via upload

--- q08.py
+++ q08.py
@@ -17,3 +17,20 @@
 ## ('9', ['A', 'B', 'C', 'E'])
 ##
 ##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+        
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+text2 = [line[1] for line in text]
+text3 = list(set(text2))
+text3.sort()
+for letra in text3:
+    x=[]
+    for line in text:
+        if line[1] == letra:
+            x.append(line[0])
+    x.sort()
+    y = (letra,x)
+    print(y)
