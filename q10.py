f45534ff5ba6acc4c28fe8bb8e665818c30d5925
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-18 18:07:40 -0500

    Add files via upload

--- q10.py
+++ q10.py
@@ -15,3 +15,24 @@
 ## jjj,18
 ##
 ##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+        
+text = [line[:-1] for line in text]
+text = [line.split('\t')[4] for line in text]
+text = ','.join(text)
+text = text.split(',')
+text2 = [line.split(':')[0] for line in text]
+text3 = [line.split(':')[1] for line in text]
+text4 = list(set(text2))
+text4.sort()
+resultado = []
+for letra in text4:
+    x=0
+    for line in text:
+        if line.split(':')[0] == letra:
+            x = x + 1
+    resultado.append('{},{}'.format(letra,x))
+resultado = '\n'.join(resultado)
+print(resultado)
