464515e4bd0dc1fa9df474fbca6aeb50b709b386
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-05-17 20:44:33 -0500

    Add files via upload

--- q03.py
+++ q03.py
@@ -8,3 +8,18 @@
 ## D,23
 ## E,67
 ##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+        
+text = [line.replace('\t',',') for line in text]
+text = [line.split(',') for line in text]
+text2 = [line[0] for line in text]
+text3 = list(set(text2))
+text3.sort()
+for letra in text3:
+    x=0
+    for line in text:
+        if line[0] == letra:
+            x = x+int(line[1])
+    print('{},{}'.format(letra,x))
