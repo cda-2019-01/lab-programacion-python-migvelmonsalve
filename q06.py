0fbf66abd885319b361740d72d8c3a8e9f0c10b4
Miguel Angel Monsalve Arango <50241610+migvelmonsalve@users.noreply.github.com>
2019-06-03 09:19:46 -0500

    Add files via upload

--- q06.py
+++ q06.py
@@ -13,3 +13,25 @@
 ## iii,2,7
 ## jjj,2,5
 ##
+text = []
+with open('data.csv', 'rt') as f:
+        text += f.readlines()
+
+text = [line[:-1] for line in text]
+text = [line.split('\t')[4] for line in text]
+text = ','.join(text)
+text = text.split(',')
+text2 = [line.split(':')[0] for line in text]
+text3 = [line.split(':')[1] for line in text]
+text4 = list(set(text2))
+text4.sort()
+for letra in text4:
+    x=int(text3[text2.index(letra)])
+    y=int(text3[text2.index(letra)])
+    for line in text:
+        if line.split(':')[0] == letra:
+            if int(line.split(':')[1]) > x:
+                x = int(line.split(':')[1])
+            elif int(line.split(':')[1]) < y:
+                y = int(line.split(':')[1])
+    print('{},{},{}'.format(letra,y,x))
